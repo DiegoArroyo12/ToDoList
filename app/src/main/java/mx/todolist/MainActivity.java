c7d19f22ec0e974d16659a82f477887b0a1cf693
AleksIvanVel <aleksvelazquez32@aragon.unam.mx>
2024-05-26 23:03:26 -0600

    ADD precios

--- app/src/main/java/mx/todolist/MainActivity.java
+++ app/src/main/java/mx/todolist/MainActivity.java
@@ -6,18 +6,29 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editTextTask;
     private ImageButton buttonAddTask;
     private LinearLayout taskContainer;
+    private Switch switchActivate;
+    private TextView totalTextView;
+    private boolean comprasActivado = false;
+    private List<EditText> priceEditTexts = new ArrayList<>();
+    private List<RelativeLayout> taskLayouts = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +37,10 @@ public class MainActivity extends AppCompatActivity {
         editTextTask = findViewById(R.id.editTextTask);
         buttonAddTask = findViewById(R.id.buttonAddTask);
         taskContainer = findViewById(R.id.taskContainer);
+        switchActivate = findViewById(R.id.switch_activate);
+        totalTextView = findViewById(R.id.totalTextView);
 
+        //Agregar tarea
         buttonAddTask.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,12 +51,23 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        //Activacion de switch
+        switchActivate.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            comprasActivado = isChecked;
+            totalTextView.setVisibility(comprasActivado ? View.VISIBLE : View.GONE);
+            for (EditText priceEditText : priceEditTexts) {
+                priceEditText.setVisibility(comprasActivado ? View.VISIBLE : View.GONE);
+            }
+            calcularTotal();
+        });
     }
 
     private void addTask(String taskText) {
         RelativeLayout taskLayout = new RelativeLayout(this);
         taskLayout.setPadding(8, 8, 8, 8);
 
+        //Text view de Descipcion de tarea
         final TextView taskEditText = new TextView(this);
         taskEditText.setText(taskText);
         taskEditText.setTextSize(18);
@@ -52,9 +77,10 @@ public class MainActivity extends AppCompatActivity {
                 RelativeLayout.LayoutParams.WRAP_CONTENT
         );
         editTextParams.addRule(RelativeLayout.ALIGN_PARENT_START);
-        editTextParams.addRule(RelativeLayout.LEFT_OF, View.generateViewId());
+        editTextParams.addRule(RelativeLayout.CENTER_VERTICAL);
         taskEditText.setLayoutParams(editTextParams);
 
+        //Boton para editar Tarea
         ImageButton editButton = new ImageButton(this);
         editButton.setImageResource(R.drawable.edit_button);
         editButton.setBackground(null);
@@ -64,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
                 RelativeLayout.LayoutParams.WRAP_CONTENT
         );
         editButtonParams.addRule(RelativeLayout.ALIGN_PARENT_END);
+        editButtonParams.addRule(RelativeLayout.CENTER_VERTICAL);
         editButton.setLayoutParams(editButtonParams);
         editButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,28 +99,56 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //EditText de precio
+        final EditText precioEditText = new EditText(this);
+
+        //Boton para eliminar Tarea
         ImageButton deleteButton = new ImageButton(this);
         deleteButton.setImageResource(R.drawable.delete_button);
         deleteButton.setBackground(null);
+        deleteButton.setId(View.generateViewId());
         RelativeLayout.LayoutParams deleteButtonParams = new RelativeLayout.LayoutParams(
                 RelativeLayout.LayoutParams.WRAP_CONTENT,
                 RelativeLayout.LayoutParams.WRAP_CONTENT
         );
         deleteButtonParams.addRule(RelativeLayout.LEFT_OF, editButton.getId());
+        deleteButtonParams.addRule(RelativeLayout.CENTER_VERTICAL);
         deleteButton.setLayoutParams(deleteButtonParams);
         deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                deleteTask(taskLayout);
+                deleteTask(taskLayout, precioEditText);
             }
         });
 
 
+        precioEditText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        precioEditText.setHint("Precio");
+        precioEditText.setVisibility(comprasActivado ? View.VISIBLE : View.GONE);
+        precioEditText.setId(View.generateViewId());
+        RelativeLayout.LayoutParams priceEditTextParams = new RelativeLayout.LayoutParams(
+                200,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+//        priceEditTextParams.addRule(taskEditText.getId());
+        priceEditTextParams.addRule(RelativeLayout.LEFT_OF,deleteButton.getId());
+        priceEditTextParams.addRule(RelativeLayout.CENTER_VERTICAL);
+        precioEditText.setLayoutParams(priceEditTextParams);
+
+        precioEditText.setOnFocusChangeListener((v, hasFocus) -> {
+            if (!hasFocus) {
+                calcularTotal();
+            }
+        });
+
         taskLayout.addView(taskEditText);
+        taskLayout.addView(precioEditText);
         taskLayout.addView(editButton);
         taskLayout.addView(deleteButton);
 
         taskContainer.addView(taskLayout);
+        priceEditTexts.add(precioEditText);
+        taskLayouts.add(taskLayout);
     }
     private void editTask(final TextView taskTextView) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -103,14 +158,33 @@ public class MainActivity extends AppCompatActivity {
         input.setInputType(InputType.TYPE_CLASS_TEXT);
         input.setText(taskTextView.getText().toString());
         builder.setView(input);
-
-        builder.setPositiveButton("OK", (dialog, which) -> taskTextView.setText(input.getText().toString()));
-        builder.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
+        builder.setTitle("Editar Tarea");
+        builder.setPositiveButton("Confirmar", (dialog, which) -> taskTextView.setText(input.getText().toString()));
+        builder.setNegativeButton("Regresar", (dialog, which) -> dialog.cancel());
 
         builder.show();
     }
 
-    private void deleteTask(final RelativeLayout taskTextView) {
-        taskContainer.removeView(taskTextView);
+    private void deleteTask(final RelativeLayout  taskLayout, final EditText priceEditText) {
+        taskContainer.removeView(taskLayout);
+        priceEditTexts.remove(priceEditText);
+        taskLayouts.remove(taskLayout);
+        calcularTotal();
     }
+
+    private void calcularTotal() {
+        double total = 0.0;
+        for (EditText priceEditText : priceEditTexts) {
+            String priceText = priceEditText.getText().toString();
+            if (!priceText.isEmpty()) {
+                try {
+                    total += Double.parseDouble(priceText);
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        totalTextView.setText("Total: " + total);
+    }
+
 }
